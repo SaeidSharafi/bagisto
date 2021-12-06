690d375cc7471ec79ccc133e15d0cf75fe5d99dd
kuro <black_assassin1991@yahoo.com>
2021-12-06 19:27:53 +0330

    init

---
+++ packages/Webkul/Admin/src/Resources/assets/js/components/navigation/nav-slide-button.vue
@@ -0,0 +1,74 @@
+<template>
+    <span class="toggle-aside-nav" @click="toggle">
+        <i class="icon" :class="iconClass"></i>
+    </span>
+</template>
+
+<style scoped>
+    .toggle-aside-nav {
+        position: absolute;
+        top: 50px;
+        right: -12px;
+    }
+    .rtl .toggle-aside-nav {
+        position: absolute;
+        top: 50px;
+        right: auto;
+        left: -12px;
+    }
+</style>
+
+<script>
+export default {
+    props: [
+        'iconClass',
+    ],
+
+    methods: {
+        toggle: function () {
+            if ($('.aside-nav').is(':visible')) {
+                this.hide();
+            } else {
+                this.show();
+            }
+        },
+
+        hide: function () {
+            let self = this;
+
+            $('.aside-nav').hide(function () {
+                if ($('body').hasClass('rtl')){
+                    $('.content-wrapper').css({
+                        marginRight: 'unset'
+                    });
+                }else {
+                    $('.content-wrapper').css({
+                        marginLeft: 'unset'
+                    });
+                }
+
+                $('#nav-expand-button').show();
+            });
+        },
+
+        show: function () {
+            let self = this;
+
+            $('#nav-expand-button').hide();
+
+            $('.aside-nav').show(function () {
+                if ($('body').hasClass('rtl')){
+                    $('.content-wrapper').css({
+                        marginRight: '280px'
+                    });
+                }else {
+                    $('.content-wrapper').css({
+                        marginLeft: '280px'
+                    });
+                }
+
+            });
+        }
+    }
+}
+</script>
---
+++ resources/admin-themes/default/assets/js/components/navigation/nav-slide-button.vue
@@ -0,0 +1,69 @@
+<template>
+    <span class="toggle-aside-nav" @click="toggle">
+        <i class="icon" :class="iconClass"></i>
+    </span>
+</template>
+
+<style scoped>
+    .toggle-aside-nav {
+        position: absolute;
+        top: 50px;
+        right: -12px;
+    }
+    .rtl .toggle-aside-nav {
+        position: absolute;
+        top: 50px;
+        right: auto;
+        left: -12px;
+    }
+</style>
+
+<script>
+export default {
+    props: [
+        'iconClass',
+    ],
+
+    methods: {
+        toggle: function () {
+            if ($('.aside-nav').is(':visible')) {
+                this.hide();
+            } else {
+                this.show();
+            }
+        },
+
+        hide: function () {
+            let self = this;
+
+            $('.aside-nav').hide(function () {
+                $('.content-wrapper').css({
+                    marginLeft: 'unset'
+                });
+
+                $('#nav-expand-button').show();
+            });
+        },
+
+        show: function () {
+            let self = this;
+
+            $('#nav-expand-button').hide();
+
+            $('.aside-nav').show(function () {
+                alert();
+                if ($('body').hasClass('rtl')){
+                    $('.content-wrapper').css({
+                        marginRight: '280px'
+                    });
+                }else {
+                    $('.content-wrapper').css({
+                        marginLeft: '280px'
+                    });
+                }
+
+            });
+        }
+    }
+}
+</script>
