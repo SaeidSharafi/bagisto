d120f82cad648a6f67646ce2d38b3e6ee56d8cc9
kuro <black_assassin1991@yahoo.com>
2022-04-09 22:54:25 +0430

    changed employment generator slug

--- resources/themes/velocity/views/home/employment-generator-courses.blade.php
+++ resources/themes/velocity/views/home/employment-generator-courses.blade.php
@@ -1,7 +1,7 @@
 <product-collections
     additional-class="yellow"
     product-id="jobsproducts-carousel"
-    product-route="{{ route('velocity.category.details', ['category-slug' => 'jobs', 'count' => 10]) }}"
+    product-route="{{ route('velocity.category.details', ['category-slug' => 'career-generator', 'count' => 10]) }}"
     locale-direction="{{ $direction }}"
     count="10">
 </product-collections>
