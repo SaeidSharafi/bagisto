c16bf65e478220cea14755040eddaf787a6755a7
kuro <black_assassin1991@yahoo.com>
2022-04-16 12:29:53 +0430

    read carousel title from category name

--- resources/themes/velocity/views/home/free-products.blade.php
+++ resources/themes/velocity/views/home/free-products.blade.php
@@ -2,7 +2,6 @@
     additional-class="gray"
     product-id="free-products-carousel"
     product-route="{{ route('velocity.category.details', ['category-slug' => 'free', 'count' => 10]) }}"
-    product-title="{{ __('shop.home.free-products') }}"
     locale-direction="{{ $direction }}"
     no-data-text="{{ __('velocity::app.products.not-available') }}"
     count="10">
