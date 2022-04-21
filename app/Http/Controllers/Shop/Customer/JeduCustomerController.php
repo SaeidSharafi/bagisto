3d4b6183bc811ee5624a34d7ce50406dace1f6bf
kuro <black_assassin1991@yahoo.com>
2022-04-21 12:40:14 +0430

    use Request class to handle data verification for Customer upodate request

--- app/Http/Controllers/Shop/Customer/JeduCustomerController.php
+++ app/Http/Controllers/Shop/Customer/JeduCustomerController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Webkul\Customer\Http\Controllers\CustomerController;
+use Webkul\Customer\Http\Requests\CustomerProfileRequest;
 use Webkul\Shop\Mail\SubscriptionEmail;
 
 class JeduCustomerController extends CustomerController
@@ -16,30 +17,17 @@ class JeduCustomerController extends CustomerController
      *
      * @return \Illuminate\Http\Response
      */
-    public function update()
+    public function update(CustomerProfileRequest $customerProfileRequest)
     {
         $isPasswordChanged = false;
-        $id = auth()->guard('customer')->user()->id;
 
-        $this->validate(request(), [
-            'first_name'            => 'required|string',
-            'last_name'             => 'required|string',
-            'gender'                => 'required',
-            'date_of_birth'         => 'date|before:today',
-            'email'                 => 'email|unique:customers,email,'.$id,
-            'password'              => 'confirmed|min:6|required_with:oldpassword',
-            'oldpassword'           => 'present',
-            'password_confirmation' => 'required_with:password',
-        ]);
+        $data = $customerProfileRequest->validated();
 
-        $data = collect(request()->input())->except('_token')->toArray();
-
-        if (isset ($data['date_of_birth']) && $data['date_of_birth'] == "") {
+        if (isset($data['date_of_birth']) && $data['date_of_birth'] == '') {
             unset($data['date_of_birth']);
         }
 
-        $data['subscribed_to_news_letter']
-            = isset($data['subscribed_to_news_letter']) ? 1 : 0;
+        $data['subscribed_to_news_letter'] = isset($data['subscribed_to_news_letter']) ? 1 : 0;
 
         if (($data['oldpassword'] != "" || $data['oldpassword'] != null)) {
             if (Hash::check($data['oldpassword'],
@@ -55,23 +43,22 @@ class JeduCustomerController extends CustomerController
                 return redirect()->back();
             }
         } else {
-            if ($data['password']){
-                if (auth()->guard('customer')->user()->password){
+            if ($data['password']) {
+                if (auth()->guard('customer')->user()->password) {
                     session()->flash('warning',
                         trans('shop::app.customer.account.profile.unmatch'));
                     return redirect()->back();
                 }
                 $data['password'] = bcrypt($data['password']);
-            }else{
+            } else {
                 unset($data['password']);
             }
 
         }
 
-
         Event::dispatch('customer.update.before');
 
-        if ($customer = $this->customerRepository->update($data, $id)) {
+        if ($customer = $this->customerRepository->update($data, auth()->guard('customer')->user()->id)) {
             if ($isPasswordChanged) {
                 Event::dispatch('user.admin.update-password', $customer);
             }
@@ -79,8 +66,7 @@ class JeduCustomerController extends CustomerController
             Event::dispatch('customer.update.after', $customer);
 
             if ($data['subscribed_to_news_letter']) {
-                $subscription
-                    = $this->subscriptionRepository->findOneWhere(['email' => $data['email']]);
+                $subscription = $this->subscriptionRepository->findOneWhere(['email' => $data['email']]);
 
                 if ($subscription) {
                     $this->subscriptionRepository->update([
@@ -105,8 +91,7 @@ class JeduCustomerController extends CustomerController
                     }
                 }
             } else {
-                $subscription
-                    = $this->subscriptionRepository->findOneWhere(['email' => $data['email']]);
+                $subscription = $this->subscriptionRepository->findOneWhere(['email' => $data['email']]);
 
                 if ($subscription) {
                     $this->subscriptionRepository->update([
@@ -116,15 +101,17 @@ class JeduCustomerController extends CustomerController
                 }
             }
 
+            $this->customerRepository->uploadImages($data, $customer);
+
             Session()->flash('success',
                 trans('shop::app.customer.account.profile.edit-success'));
 
             return redirect()->route($this->_config['redirect']);
-        } else {
-            Session()->flash('success',
-                trans('shop::app.customer.account.profile.edit-fail'));
-
-            return redirect()->back($this->_config['redirect']);
         }
+        Session()->flash('success',
+            trans('shop::app.customer.account.profile.edit-fail'));
+
+        return redirect()->back($this->_config['redirect']);
+
     }
 }
