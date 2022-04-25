f1ae1d777ffb2e45fa99cbd320ca89de99acfb57
kuro <black_assassin1991@yahoo.com>
2022-04-25 15:41:08 +0430

    fix virtual courses carousel element has wrong id name

--- resources/themes/velocity/views/home/virtual-courses.blade.php
+++ resources/themes/velocity/views/home/virtual-courses.blade.php
@@ -1,6 +1,6 @@
 <product-collections
     additional-class="purple"
-    product-id="jobs-products-carousel"
+    product-id="virtual-products-carousel"
     product-route="{{ route('velocity.category.details', ['category-slug' => 'virtual', 'count' => 10]) }}"
     locale-direction="{{ $direction }}"
     count="10">
