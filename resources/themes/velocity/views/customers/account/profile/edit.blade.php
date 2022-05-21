a9dca79ea25006f0e2083610a4940953317f9ca9
kuro <black_assassin1991@yahoo.com>
2022-05-21 13:19:33 +0430

    profile edit form should show server errors

--- resources/themes/velocity/views/customers/account/profile/edit.blade.php
+++ resources/themes/velocity/views/customers/account/profile/edit.blade.php
@@ -8,6 +8,7 @@
     <div class="account-head mb-15">
         <span class="back-icon"><a href="{{ route('customer.account.index') }}"><i class="icon icon-menu-back"></i></a></span>
         <span class="account-heading">{{ __('shop::app.customer.account.profile.index.title') }}</span>
+
         <span></span>
     </div>
 
@@ -16,9 +17,11 @@
     <form
         method="POST"
         @submit.prevent="onSubmit"
-        action="{{ route('customer.profile.store') }}">
+        action="{{ route('customer.profile.store') }}"
+        enctype="multipart/form-data">
 
         <div class="account-table-content edit-account">
+
             @csrf
 
             {!! view_render_event('bagisto.shop.customers.account.profile.edit_form_controls.before', ['customer' => $customer]) !!}
@@ -31,6 +34,8 @@
                 <div class="col-12">
                     <input value="{{ $customer->first_name }}" name="first_name" type="text" v-validate="'required'" data-vv-as="&quot;{{ __('shop::app.customer.account.profile.fname') }}&quot;" />
                     <span class="control-error" v-if="errors.has('first_name')" v-text="errors.first('first_name')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('first_name') }}" v-text="'{{$errors->first('first_name')}}'"></span>
+
                 </div>
             </div>
 
@@ -44,6 +49,7 @@
                 <div class="col-12">
                     <input value="{{ $customer->last_name }}" name="last_name" type="text" v-validate="'required'" data-vv-as="&quot;{{ __('shop::app.customer.account.profile.lname') }}&quot;" />
                     <span class="control-error" v-if="errors.has('last_name')" v-text="errors.first('last_name')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('last_name') }}" v-text="'{{$errors->first('last_name')}}'"></span>
                 </div>
             </div>
 
@@ -61,9 +67,14 @@
                         class="control styled-select"
                         data-vv-as="&quot;{{ __('shop::app.customer.account.profile.gender') }}&quot;">
 
-                        <option value=""  @if ($customer->gender == "") selected @endif></option>
-                        <option
-                            value="Other"
+                        <option value=""
+                            @if ($customer->gender == "")
+                                selected="selected"
+                            @endif>
+                            {{ __('admin::app.customers.customers.select-gender') }}
+                        </option>
+
+                        <option value="Other"
                             @if ($customer->gender == "Other")
                                 selected="selected"
                             @endif>
@@ -110,6 +121,9 @@
                                    initial-value="{{  old('date_of_birth') ?? $customer->date_of_birth}}"
                                    placeholder="{{ trans('shop::app.customer.account.profile.dob') }}"></p-date-picker>
 
+                    <span class="control-error" v-if="errors.has('date_of_birth')" v-text="errors.first('date_of_birth')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('date_of_birth') }}" v-text="'{{$errors->first('date_of_birth')}}'"></span>
+
                 </div>
             </div>
 
@@ -123,6 +137,7 @@
                 <div class="col-12">
                     <input value="{{ $customer->email }}" name="email" type="text" />
                     <span class="control-error" v-if="errors.has('email')" v-text="errors.first('email')"></span>
+                    <span class="control-error" v-if="{{$errors->has('email')}}" v-text="'{{$errors->first('email')}}'"></span>
                 </div>
             </div>
 
@@ -136,10 +151,29 @@
                 <div class="col-12">
                     <input value="{{ old('phone') ?? $customer->phone }}" name="phone" type="text" v-validate="'required'"/>
                     <span class="control-error" v-if="errors.has('phone')" v-text="errors.first('phone')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('phone') }}" v-text="'{{$errors->first('phone')}}'"></span>
                 </div>
             </div>
 
             {!! view_render_event('bagisto.shop.customers.account.profile.edit.phone.after', ['customer' => $customer]) !!}
+
+            <div class="row image-container {!! $errors->has('image.*') ? 'has-error' : '' !!}">
+                <label class="col-12">
+                    {{ __('admin::app.catalog.categories.image') }}
+                </label>
+
+                <div class="col-12">
+                    <image-wrapper :button-label="'{{ __('admin::app.catalog.products.add-image-btn-title') }}'" input-name="image" :multiple="false" :images='"{{ $customer->image_url }}"'></image-wrapper>
+
+                    <span class="control-error" v-if="{!! $errors->has('image.*') !!}">
+                        @foreach ($errors->get('image.*') as $key => $message)
+                            @php echo str_replace($key, 'Image', $message[0]); @endphp
+                        @endforeach
+                    </span>
+                </div>
+            </div>
+
+            {!! view_render_event('bagisto.shop.customers.account.profile.edit.image.after', ['customer' => $customer]) !!}
 
             <div class="row">
                 <label class="col-12">
@@ -164,9 +198,10 @@
                         name="password"
                         ref="password"
                         type="password"
-                        v-validate="'min:6|max:18'" />
+                        v-validate="'min:6'" />
 
                     <span class="control-error" v-if="errors.has('password')" v-text="errors.first('password')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('password') }}" v-text="'{{$errors->first('password')}}'"></span>
                 </div>
             </div>
 
@@ -182,6 +217,7 @@
                     v-validate="'min:6|confirmed:password'" data-vv-as="confirm password" />
 
                     <span class="control-error" v-if="errors.has('password_confirmation')" v-text="errors.first('password_confirmation')"></span>
+                    <span class="control-error" v-if="{{ $errors->has('password_confirmation') }}" v-text="'{{$errors->first('password_confirmation')}}'"></span>
                 </div>
             </div>
 
