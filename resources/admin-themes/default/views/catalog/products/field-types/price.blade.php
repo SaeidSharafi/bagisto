b2e73c00fd221a65b04b545f0687d7e4cb0baa6f
kuro <black_assassin1991@yahoo.com>
2022-06-05 17:32:08 +0430

    only use floor if the price value is not null

--- resources/admin-themes/default/views/catalog/products/field-types/price.blade.php
+++ resources/admin-themes/default/views/catalog/products/field-types/price.blade.php
@@ -1,3 +1,9 @@
+@php
+    $value= old($attribute->code) ?: $product[$attribute->code];
+   if (!is_null($value)){
+      $value= floor($value);
+   }
+@endphp
 <input type="text" v-validate="'{{$validations}}'" class="control" id="{{ $attribute->code }}"
-       name="{{ $attribute->code }}" value="{{ floor(old($attribute->code) ?: $product[$attribute->code])}}"
+       name="{{ $attribute->code }}" value="{{$value }}"
        data-vv-as="&quot;{{ $attribute->admin_name }}&quot;"/>
