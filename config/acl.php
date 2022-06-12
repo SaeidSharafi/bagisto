a4e1d1440eaba2f0e1384b14dfca7fe19233f7e9
kuro <black_assassin1991@yahoo.com>
2022-06-12 11:33:36 +0430

    add order complete route permission

--- config/acl.php
+++ config/acl.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-
+    [
+        'key'   => 'sales.orders.complete',
+        'name'  => 'admin.datagrid.complete',
+        'route' => 'admin.sales.orders.complete',
+        'sort'  => 1,
+    ]
 ];
