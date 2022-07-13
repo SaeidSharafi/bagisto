f793c9c2d4c8dd2a7e8781fdbe706b4374e33ceb
kuro <black_assassin1991@yahoo.com>
2022-07-13 12:14:09 +0430

    add prefix for config variables

--- resources/themes/velocity/views/layouts/header/index.blade.php
+++ resources/themes/velocity/views/layouts/header/index.blade.php
@@ -18,8 +18,9 @@
 
                             </sidebar-header>
                         </li>
+
                         <li class="top-level"><a href="{{route("customer.auth.create")}}" target="_self">درباره ما</a></li>
-                        <li class="top-level"><a href="{{config('blog_url')}}" target="_blank">بلاگ</a></li>
+                        <li class="top-level"><a href="{{config('app.blog_url')}}" target="_blank">بلاگ</a></li>
                         @guest('customer')
                         <li class="top-level login"><a href="{{route("customer.auth.create")}}" target="_self">ورود</a></li>
                         @endguest
