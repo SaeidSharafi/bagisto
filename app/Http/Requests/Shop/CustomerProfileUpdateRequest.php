4d85091705db08961b972915f865de332424cd82
kuro <black_assassin1991@yahoo.com>
2022-08-01 10:47:54 +0430

    national code should be unique in validation

--- app/Http/Requests/Shop/CustomerProfileUpdateRequest.php
+++ app/Http/Requests/Shop/CustomerProfileUpdateRequest.php
@@ -37,12 +37,31 @@ class CustomerProfileUpdateRequest extends FormRequest
             'oldpassword'           => 'present',
             'password_confirmation' => 'required_with:password',
             'image.*'               => 'mimes:bmp,jpeg,jpg,png,webp',
-            'national_code'         => ['filled',
+            'national_code'         => ['filled','unique:customers,national_code',
                                         Rule::when(function ($input) {
                                             return !isset($input->is_foreign);
                                         },new Nationalcode)],
             'father_name'           => 'nullable',
             'education_field'       => 'nullable',
+        ];
+
+    }
+
+    public function attributes()
+    {
+        return [
+            'first_name'            => __('shop::app.customer.account.profile.fname'),
+            'last_name'             => __('shop::app.customer.account.profile.lname'),
+            'gender'                => __('shop::app.customer.account.profile.gender'),
+            'date_of_birth'         => __('shop::app.customer.account.profile.dob'),
+            'email'                 => __('shop::app.customer.account.profile.email'),
+            'password'              => __('velocity::app.shop.general.new-password'),
+            'oldpassword'           => __('velocity::app.shop.general.enter-current-password'),
+            'password_confirmation' => __('velocity::app.shop.general.confirm-new-password'),
+            'image.*'               => __('admin::app.catalog.categories.image'),
+            'national_code'         => __('app.customer.account.profile.national_code'),
+            'father_name'           => __('app.customer.account.profile.father_name'),
+            'education_field'       => __('app.customer.account.profile.education_field') ,
         ];
     }
 }
