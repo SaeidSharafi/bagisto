96b818ef1a192c17e3fe9d4836165fd942957312
saeid sharafi <saeed_sharafi12@yahoo.com>
2022-11-28 17:17:03 +0330

    fix enamd background

--- resources/views/shop/footer/enamad.blade.php
+++ resources/views/shop/footer/enamad.blade.php
@@ -2,7 +2,7 @@
     <div class="block-title"></div>
     <div class="block-content">
         <a referrerpolicy="origin" target="_blank"
-           class="bg-white rounded"
+           class="bg-white rounded d-inline-block"
            href="https://trustseal.enamad.ir/?id=272790&Code=1MSwalgoWs2TeK8lftli">
             <img referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=272790&Code=1MSwalgoWs2TeK8lftli"
                  alt="" style="cursor:pointer"
