8738b7c94a9ecf9ddd547345572f1fcd7095c3c7
saeed <saeed_sharafi12@yahoo.com>
2023-11-05 11:52:11 +0330

    add reset-password permission

--- config/acl.php
+++ config/acl.php
@@ -6,5 +6,11 @@ return [
         'name'  => 'admin.datagrid.complete',
         'route' => 'admin.sales.orders.complete',
         'sort'  => 1,
+    ],
+    [
+        'key' => 'customers.customers.reset-password',
+        'name' => 'shop::app.customer.reset-password.title',
+        'route' => 'admin.customers.reset-password',
+        'sort' => 1,
     ]
 ];
