e8e47b724973f459a4055aacddf7050eb9e2e1db
saeed <saeed_sharafi12@yahoo.com>
2024-01-31 14:34:14 +0330

    fix use order total instead of order_item total

--- app/Services/HttpRequestService.php
+++ app/Services/HttpRequestService.php
@@ -14,7 +14,6 @@ class HttpRequestService
     protected $order;
     protected $operation;
 
-
     const OP_UPDATE_REGISTERATION = 1;
     const OP_REGISTER_STUDENT = 2;
     const SYSTEM_USER_ID = 1013;
@@ -38,7 +37,7 @@ class HttpRequestService
 
     public function UpdateRegisteration()
     {
-        if(! $api_key = config('app.ims.api_key')) {
+        if (!$api_key = config('app.ims.api_key')) {
             throw new \InvalidArgumentException(__('app.response.sync-ims-api-key'));
         }
         $registrations = [];
@@ -52,7 +51,7 @@ class HttpRequestService
         }
 
         foreach ($this->order->items as $item) {
-            if(!$item->product_number) {
+            if (!$item->product_number) {
                 throw new \InvalidArgumentException(__('app.response.sync-ims-porduct-number'));
             }
         }
@@ -71,13 +70,13 @@ class HttpRequestService
             'student'            => [
                 'national_code'  => $customer->national_code,
                 'created'        => $customer->created_at->format('Y-m-d h:i:s'),
-                'first_name'   => $customer->first_name,
-                'last_name' => $customer->last_name,
-                'phone'            => $customer->phone,
-                'note'        => $customer->notes,
+                'first_name'     => $customer->first_name,
+                'last_name'      => $customer->last_name,
+                'phone'          => $customer->phone,
+                'note'           => $customer->notes,
                 'field_of_study' => $customer->education_field,
                 'father_name'    => $customer->father_name,
-                'date_of_birth'       => $date_of_birth,
+                'date_of_birth'  => $date_of_birth,
                 'gender'         => ($customer->gender == 'Male') ? 1 : 2,
             ],
 
@@ -96,15 +95,15 @@ class HttpRequestService
 
             $registration = [
                 'course_code' => $product_number,
-                'payment'   => [
-                    'discount_type'  => $item->base_discount_amount > 0 ? 'manual' : 'none',
-                    'discount_amount' => (int)$item->discount_amount,
-                    'discount_code' => $item->coupon_code,
-                    'amount'  => (int)$item->grand_total,
-                    'bill' => $this->order->increment_id,
-                    'date' => $item->created_at->format('Y-m-d'),
+                'payment'     => [
+                    'discount_type'   => $item->base_discount_amount > 0 ? 'manual' : 'none',
+                    'discount_amount' => (int) $item->discount_amount,
+                    'discount_code'   => $this->order->coupon_code,
+                    'amount'          => (int) $this->order->grand_total,
+                    'bill'            => $this->order->increment_id,
+                    'date'            => $item->created_at->format('Y-m-d'),
                 ],
-                'note'       =>  $comments,
+                'note'        => $comments,
             ];
             $registrations['registeraions'][] = $registration;
         }
@@ -112,7 +111,7 @@ class HttpRequestService
             'data' => $registrations
         ];
 
-        $url = config('app.ims.base_url') . '/api/v1/enrol';
+        $url = config('app.ims.base_url').'/api/v1/enrol';
 
         $respons = Http::withToken($api_key)
             ->asForm()
@@ -120,11 +119,11 @@ class HttpRequestService
 
         if ($respons->ok()) {
             $enrolment = $respons->json('enrolment');
-            if(!$this->order->ims_synced_at) {
+            if (!$this->order->ims_synced_at) {
                 Order::where('id', $this->order->id)->update([
-                    'ims_synced_at' => $enrolment['created_at'],
+                    'ims_synced_at'    => $enrolment['created_at'],
                     'ims_enrolment_id' => $enrolment['id']
-                    ]);
+                ]);
             }
             return true;
         }
@@ -135,7 +134,7 @@ class HttpRequestService
             throw new AuthenticationException($respons->json('message'));
         }
         if ($respons->failed()) {
-            Log::error('Registring enrolment in IMS failed: \n' . $respons->body());
+            Log::error('Registring enrolment in IMS failed: \n'.$respons->body());
         }
 
         return false;
