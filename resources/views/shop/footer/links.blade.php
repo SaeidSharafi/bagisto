917a748970fe7c52694a198c3711961d761a883b
Saeid Sharafi <saeed_sharafi12@yahoo.com>
2024-07-11 12:06:34 +0330

    Update links.blade.php

--- resources/views/shop/footer/links.blade.php
+++ resources/views/shop/footer/links.blade.php
@@ -3,18 +3,8 @@
     <div class="block-content">
         <ul class="links">
             <li><em class="porto-icon-right-dir theme-color"></em>
-                <a target="_blank" href="http://qaz.acecr.ac.ir/fa" title="ستاد">ستاد</a></li>
-            <li><em class="porto-icon-right-dir theme-color"></em>
-                <a target="_blank" href="http://jdfarhangi.ir/Default.aspx?bcode=30" title="معاونت فرهنگی">معاونت فرهنگی</a>
-            </li>
-            <li><em class="porto-icon-right-dir theme-color"></em>
-                <a target="_blank" href="/" title="معاونت پژوهشی">معاونت پژوهشی</a>
-            </li>
-            <li><em class="porto-icon-right-dir theme-color"></em>
-                <a target="_blank" href="http://jdqazvin.ir/artcenter/#home" title="هنر و معماری">هنر و معماری</a></li>
-            <li><em class="porto-icon-right-dir theme-color"></em>
-                <a target="_blank" href="http://qaz.acecr.ac.ir/fa/page/2693/%D9%85%D8%B1%DA%A9%D8%B2-%D8%B9%D9%84%D9%85%DB%8C-%DA%A9%D8%A7%D8%B1%D8%A8%D8%B1%D8%AF%DB%8C-%D8%AC%D9%87%D8%A7%D8%AF%D8%AF%D8%A7%D9%86%D8%B4%DA%AF%D8%A7%D9%87%DB%8C-%D9%82%D8%B2%D9%88%DB%8C%D9%86" title="مرکز علمی کاربردی">مرکز علمی کاربردی</a></li>
-            <li><em class="porto-icon-right-dir theme-color"></em>
+                <a target="_blank" href="http://qaz.acecr.ac.ir/fa" title="دفتر مرکزی جهاد دانشگاهی قزوین">دفتر مرکزی جهاد دانشگاهی قزوین</a></li>
+           <li><em class="porto-icon-right-dir theme-color"></em>
                 <a target="_blank" href="http://jdrouyesh.ir/rooyesh" title="مشاهده نمرات">مشاهده نمرات</a></li>
         </ul>
     </div>
