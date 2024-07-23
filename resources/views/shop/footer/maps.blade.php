42ae05eefd886cc254eaa59a7cb32069ba095d6e
saeed <saeed_sharafi12@yahoo.com>
2024-07-23 11:19:16 +0330

    remove map overlay

--- resources/views/shop/footer/maps.blade.php
+++ resources/views/shop/footer/maps.blade.php
@@ -6,14 +6,14 @@
         <div class="col-md-6">
             <span class="d-block text-center font-weight-bold mb-2" style="font-size: 1.5rem;">مرکز یک</span>
             <p class="text-center" style="font-size: 1.4rem;">قزوین، چهارراه ولیعصر(عج) جنب بانک سپه</p>
-            <div class="map-responsive">
+            <div class="map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3825.6127611162306!2d50.01571669690081!3d36.265137102488914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8cabae9340c629%3A0xb1ccaa0c32618c30!2z2KzZh9in2K8g2K_Yp9mG2LTar9in2YfbjCDZgtiy2YjbjNmG!5e0!3m2!1sen!2sau!4v1718101157659!5m2!1sen!2sau" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="col-md-6 second-map">
             <span class="d-block text-center font-weight-bold mb-2" style="font-size: 1.6rem;">مرکز دو</span>
             <p class="text-center" style="font-size: 1.4rem;">قزوین، سه راه خیام، جنب پاساژ علوی</p>
-            <div class="map-responsive">
+            <div class="map">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1137.312347207791!2d50.00703047334678!3d36.268510640060235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ff355b73ef9bc19%3A0x6e572021cd40ce19!2z2KLZhdmI2LLYtNqv2KfZhyDYstio2KfZhiDYrNmH2KfYryDYr9in2YbYtNqv2KfZh9uMINmC2LLZiNuM2YY!5e0!3m2!1sen!2sau!4v1718101302780!5m2!1sen!2sau" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
