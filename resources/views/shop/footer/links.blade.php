f562a47b10898241b26518c135b99d04bfc2214a
saeed <saeed_sharafi12@yahoo.com>
2024-07-28 17:59:05 +0330

    remove footer links

--- resources/views/shop/footer/links.blade.php
+++ resources/views/shop/footer/links.blade.php
@@ -5,11 +5,7 @@
             {!! $velocityMetaData->footer_left_content !!}
         @else
             <ul class="links">
-                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://qaz.acecr.ac.ir/fa" title="ستاد" rel="noopener noreferrer">ستاد</a></li>
-                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://jdfarhangi.ir/Default.aspx?bcode=30" title="معاونت فرهنگی" rel="noopener noreferrer">معاونت فرهنگی</a></li>
-                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="/" title="معاونت پژوهشی" rel="noopener noreferrer">معاونت پژوهشی</a></li>
-                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://jdqazvin.ir/artcenter/#home" title="هنر و معماری" rel="noopener noreferrer">هنر و معماری</a></li>
-                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://qaz.acecr.ac.ir/fa/page/2693/%D9%85%D8%B1%DA%A9%D8%B2-%D8%B9%D9%84%D9%85%DB%8C-%DA%A9%D8%A7%D8%B1%D8%A8%D8%B1%D8%AF%DB%8C-%D8%AC%D9%87%D8%A7%D8%AF%D8%AF%D8%A7%D9%86%D8%B4%DA%AF%D8%A7%D9%87%DB%8C-%D9%82%D8%B2%D9%88%DB%8C%D9%86" title="مرکز علمی کاربردی" rel="noopener noreferrer">مرکز علمی کاربردی</a></li>
+                <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://qaz.acecr.ac.ir/fa" title="دفتر مرکزی جهاد دانشگاهی قزوین" rel="noopener noreferrer">دفتر مرکزی جهاد دانشگاهی قزوین</a></li>
                 <li><em class="porto-icon-right-dir theme-color"></em> <a target="_blank" href="http://jdrouyesh.ir/rooyesh" title="مشاهده نمرات" rel="noopener noreferrer">مشاهده نمرات</a></li>
             </ul>
         @endif
