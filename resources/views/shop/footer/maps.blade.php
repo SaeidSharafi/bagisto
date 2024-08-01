fac3db84fbfa54c3179849d076d1145a29b7b2d9
saeed <saeed_sharafi12@yahoo.com>
2024-08-01 12:14:03 +0330

    fix map size

--- resources/views/shop/footer/maps.blade.php
+++ resources/views/shop/footer/maps.blade.php
@@ -14,7 +14,7 @@
             <span class="d-block text-center font-weight-bold mb-2" style="font-size: 1.6rem;">مرکز دو</span>
             <p class="text-center" style="font-size: 1.4rem;">قزوین، خیابان دانشگاه، بلوار شهید بابایی، کوچه خاکعلی، پ ۱۵</p>
             <div class="map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d200.97660267482783!2d50.017139017581954!3d36.29713229591493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ff355586f051619%3A0x6d0607b5b1c64dc9!2sJahad%20Daneshgahi%20of%20Qazvin!5e0!3m2!1sen!2suk!4v1722497486928!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d200.97660267482783!2d50.017139017581954!3d36.29713229591493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ff355586f051619%3A0x6d0607b5b1c64dc9!2sJahad%20Daneshgahi%20of%20Qazvin!5e0!3m2!1sen!2suk!4v1722497486928!5m2!1sen!2suk" width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
