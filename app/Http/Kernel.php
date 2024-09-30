1313393e539a3d07a47cf90ce1e8f1eddf0c4eeb
saeed <saeed_sharafi12@yahoo.com>
2024-09-30 11:47:20 +0330

    add Trim String

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\SecureHeaders::class,
+        TrimStrings::class,
     ];
 
     /**
