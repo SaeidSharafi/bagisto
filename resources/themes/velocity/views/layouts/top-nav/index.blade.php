22ab31873bb0948eafa6e10eaa2146b36737b367
saeed <saeed_sharafi12@yahoo.com>
2024-10-01 13:48:45 +0330

    add alt to logos

--- resources/themes/velocity/views/layouts/top-nav/index.blade.php
+++ resources/themes/velocity/views/layouts/top-nav/index.blade.php
@@ -5,6 +5,14 @@
         if ($advertisement)
         $advertisement_path = asset('/storage/' .$advertisement);
         }
+    $channel = core()->getCurrentChannel();
+
+    $homeSEO = $channel->home_seo;
+    $metaTitle = $channel->name;
+    if (isset($homeSEO)) {
+        $homeSEO = json_decode($channel->home_seo);
+        $metaTitle = $homeSEO->meta_title;
+        }
 @endphp
 @if($advertisement_path)
     <div class="w-100 top-bar-gif">
@@ -15,15 +23,15 @@
 <nav class="row align-items-stretch justify-content-around" id="top">
     <div class="col-sm-6">
         <a class="navbar-brand" href="{{ route('shop.home.index') }}" aria-label="Logo">
-            <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('images/logo-text.png') }}"
-                 alt=""/>
+            <img class="logo" src="{{ $channel->logo_url ?? asset('images/logo-text.png') }}"
+                 alt="{{$metaTitle}}"/>
         </a>
     </div>
 
     <div class="col-sm-6">
         <div class="align-items-center d-flex h-100 justify-content-end w-100">
-            <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('images/moto.png') }}?v=1.1"
-                 alt=""/>
+            <img class="logo" src="{{ $channel->logo_url ?? asset('images/moto.png') }}?v=1.1"
+                 alt="{{$channel->name}}"/>
         </div>
     </div>
 </nav>
