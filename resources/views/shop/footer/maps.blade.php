ed06247368026bb7baf0cda560972d1d70d5b18c
saeed <saeed_sharafi12@yahoo.com>
2026-02-02 15:57:53 +0330

    fix: map size

--- resources/views/shop/footer/maps.blade.php
+++ resources/views/shop/footer/maps.blade.php
@@ -7,14 +7,17 @@
             <span class="d-block text-center font-weight-bold mb-2" style="font-size: 1.5rem;">مرکز یک</span>
             <p class="text-center" style="font-size: 1.4rem;">قزوین، چهارراه ولیعصر(عج) جنب بانک سپه</p>
             <div class="map">
-                <iframe src="https://balad.ir/embed?p=4p98t93p6hUnOX" title="مشاهده «مرکز آموزش شماره ۱ جهاد دانشگاهی» روی نقشه بلد" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                <iframe src="https://balad.ir/embed?p=4p98t93p6hUnOX"
+                        title="مشاهده «مرکز آموزش شماره ۱ جهاد دانشگاهی» روی نقشه بلد"
+                        width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
         <div class="col-md-6 second-map">
             <span class="d-block text-center font-weight-bold mb-2" style="font-size: 1.6rem;">مرکز دو</span>
             <p class="text-center" style="font-size: 1.4rem;">قزوین، خیابان دانشگاه، بلوار شهید بابایی، کوچه خاکعلی، پ ۱۵</p>
             <div class="map">
-                <iframe src="https://balad.ir/embed?p=1yOn8MVbjk77Kw" title="مشاهده «مرکز آموزش های کوتاه مدت ۲ جهاد دانشگاهی» روی نقشه بلد" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>            </div>
+                <iframe src="https://balad.ir/embed?p=1yOn8MVbjk77Kw" title="مشاهده «مرکز آموزش های کوتاه مدت ۲ جهاد دانشگاهی» روی نقشه بلد"
+                        width="100%" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </div>
         </div>
     </div>
 </div>
